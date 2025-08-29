bb6096648d5542e99f38c5beccfec13855db3cd0
sopp1313 <dldpdms0131@gmail.com>
2025-08-29 17:01:12 +0900

    feat: 개인페이지구현

--- src/main/java/com/network/calendar_api/repository/EventRepository.java
+++ src/main/java/com/network/calendar_api/repository/EventRepository.java
@@ -13,14 +13,11 @@ import java.util.Optional;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
-List<Event> findByDateBetween(LocalDate start, LocalDate end);
-List<Event> findByEventType(EventType eventType);
-List<Event> findByMemberAndEventType(Member member, EventType eventType);
-List<Event> findByMember(Member member);
-
-@Query("SELECT e FROM Event e WHERE e.eventType ='PUBLIC' AND e.date LIKE :yearMonth% ")
-List<Event> findPublicEventByYearMonth(@Param("yearMonth") Integer yearMonth);
-
-@Query("SELECT e FROM Event e WHERE e.eventType ='PERSONAL' AND e.date LIKE :yearMonth% ")
-List<Event> findPersonalEventByMembetAndYearMonth(@Param("membet") Member member,@Param("yearMonth") Integer yearMonth);
+    List<Event> findByDateBetween(LocalDate start, LocalDate end);
+    List<Event> findByEventType(EventType eventType);
+    List<Event> findByMemberAndEventType(Member member, EventType eventType);
+    List<Event> findByMember(Member member);
+    List<Event> findByEventTypeAndDateBetween(EventType eventType, LocalDate start, LocalDate end);
+    List<Event> findByMemberAndEventTypeAndDateBetween(Member member, EventType eventType, LocalDate start, LocalDate end);
 }
+
